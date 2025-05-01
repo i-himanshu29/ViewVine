cd1d6c4255820db0ccf346d8a76a5a9dd72822d8
Himanshu Maurya <mauryahimanshu979@gmail.com>
2025-05-01 20:24:48 +0530

    health controller done and tested

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -3,7 +3,17 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
-  //TODO: build a healthcheck response that simply returns the OK status as json with a message
+  try {
+    console.log("Checking database connection...");
+   
+    res.status(200).json(new ApiResponse(200, { message: "Server is running" }));
+  } catch (error) {
+    console.error("Healthcheck failed:", error);
+   
+    res
+      .status(500)
+      .json(new ApiError(500, "Server is not running due to an internal error"));
+  }
 });
 
 export { healthcheck };
